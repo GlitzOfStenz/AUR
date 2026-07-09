184a78a14203fc37ee1119fa7f6ebbb4093571ab
Mariappan <mariappan15122005@gmail.com>
2026-07-09 18:33:49 +0530

    fix: import useSidebar in Homepage

--- src/app/components/Homepage.tsx
+++ src/app/components/Homepage.tsx
@@ -28,6 +28,7 @@ import {
 import { FEATURED_ARTICLES, University, Article } from "../data";
 import { BLOG_CATEGORY_TABS, getPublishedStoredBlogs, storedBlogToArticle } from "../lib/blog-storage";
 import { useUniversityData } from "./data/UniversityDataProvider";
+import { useSidebar } from "./navigation/SidebarContext";
 import "./home/ref-home.css";
 import { API_BASE_URL } from "../lib/universities";
 
